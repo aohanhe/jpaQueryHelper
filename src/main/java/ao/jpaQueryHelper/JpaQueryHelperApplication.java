24a64fc10ab95dc461ba0ee5fd3b742efe59886c
aohanhe <aohanhe@163.com>
2018-08-27 16:50:02 +0800

    创建工程

---
+++ src/main/java/ao/jpaQueryHelper/JpaQueryHelperApplication.java
@@ -0,0 +1,12 @@
+package ao.jpaQueryHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaQueryHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaQueryHelperApplication.class, args);
+	}
+}
